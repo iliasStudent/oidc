171032a1e07c5f0fd2e59c06fd6396f5177e392e
Mayk Dimov <mayk.dimov@student.ap.be>
2024-08-15 18:52:38 +0200

    nieuwe API key en Auth0 credentials

--- angularauth0/src/app/http.service.ts
+++ angularauth0/src/app/http.service.ts
@@ -10,10 +10,11 @@ import { weatherData } from './weatherdata';
 export class HttpService {
 
   private apiUrl = 'http://localhost:3000/api/weather/';
-  private apiKey = '6a060dcf7d41496db2802622231811';
+  private apiKey = '270fd59ba8804655885135026241508';
 
   constructor(private http: HttpClient) { }
   getCurrentWeather(city: string, access_token: any): Observable<any> {
+    console.log("DAT" + access_token);
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${access_token} `
     });
@@ -21,29 +22,31 @@ export class HttpService {
   }
 
   getAccessToken(authorizationCode: string): Observable<any> {
+    console.log("AUTHCODE: " + authorizationCode);
     const body = new URLSearchParams();
     body.set('grant_type', 'authorization_code');
-    body.set('client_id', 'lDpFkylmWzyauZsuGuYlHD8zx9uWZ3rd');
-    body.set('client_secret', 'P_A47XvfiiLGpNEw4_QBmyW1HufTpUB2_ITZE2FsPX2rZuhpp0yaUluft7ww-Bfo');
+    body.set('client_id', 'mCYR2dStiPYfyDqK5tMKgm455mHvrEBU');
+    body.set('client_secret', 'NhtRebXme1uk4tWX6ITvEaxzmFCVIAoCMKDqDUA22jfBC_wLzjXbamcwrxOa7NSJ');
     body.set('code', authorizationCode);
     body.set('redirect_uri', "http://localhost:4200/callbackauth");
     body.set('state','Bankoko');
     const headers = new HttpHeaders()
       .set('Content-Type', 'application/x-www-form-urlencoded');
+      console.log(this.http.post("https://dev-7qvir82wwwaaewg7.us.auth0.com/oauth/token/", body.toString(), { headers: headers }));
 
-    return this.http.post("https://dev-vjf815mo6cnziql2.eu.auth0.com/oauth/token/", body.toString(), { headers: headers });
+    return this.http.post("https://dev-7qvir82wwwaaewg7.us.auth0.com/oauth/token/", body.toString(), { headers: headers });
   }
 
   login(): Observable<any> {
     const params = new URLSearchParams();
     params.set('response_type', 'code');
-    params.set('client_id', 'lDpFkylmWzyauZsuGuYlHD8zx9uWZ3rd');
+    params.set('client_id', 'mCYR2dStiPYfyDqK5tMKgm455mHvrEBU');
     params.set('redirect_uri', 'http://localhost:4200/callbackauth');
     params.set('scope', 'openid profile email');
     params.set('audience', 'http://localhost:4200/weather');
     params.set('state', 'Bankoko');
   
-    const authorizationUrl = `https://dev-vjf815mo6cnziql2.eu.auth0.com/authorize?${params.toString()}`;
+    const authorizationUrl = `https://dev-7qvir82wwwaaewg7.us.auth0.com/authorize?${params.toString()}`;
     
     window.location.href = authorizationUrl;
     return of(null); 
